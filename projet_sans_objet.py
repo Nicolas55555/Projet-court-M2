0c7b75329d101a42f21cc2d5152f81fa15576911
AudeSt <70609766+AudeSt@users.noreply.github.com>
2020-09-16 22:12:17 +0200

    Documentation
    Ajout de commentaires et de description des fonctions

--- projet_sans_objet.py
+++ projet_sans_objet.py
@@ -150,6 +150,20 @@ ax.scatter(points_sphere_coord["x"],points_sphere_coord["y"],points_sphere_coord
 
 def EquationPlan(CentreSphere,PointeVecteur):
     
+	'''
+	Retourne les paramètres a,b,c,d définissant l'équation de plan de type a*x+b*y+c*z+d=0, avec x,y,z des nombres réels.
+	
+	ARGUMENTS
+	----------
+	CentreSphere : coordonnées du centre de la sphère définie plus haut, au format [x0,y0,z0].
+	PointeVecteur : coordonnées de l'extrémité du vecteur normal au plan, au format [x1,y1,z1].
+	
+	SORTIES
+	----------
+	Parametres=[a,b,c,d]
+	
+	'''
+	
     vecteur_directeur=[PointeVecteur[i]-CentreSphere[i] for i in range(3)]
     
     #coordonnées centre sphère
@@ -166,9 +180,20 @@ def EquationPlan(CentreSphere,PointeVecteur):
     return Parametres
 
 def DeterminerPlanSuivant(CentreSphere,Parametres,EPAISSEUR):
-    '''
-    donne le deuxième plan de la tranche
-    '''	
+   	'''
+	Dans le but de définir une "tranche" dans l'espace composée de deux plans, cette fonction définit les paramètres du second plan en fonction du premier.
+	
+	ARGUMENTS
+	----------
+	CentreSphere : coordonnées du centre de la sphère définie plus haut, au format [x0,y0,z0].
+	Parametres : parametres au format [a,b,c,d] de l'équation définissant le premier plan.
+	EPAISSEUR : distance entre les deux plans parallèles.
+	
+	SORTIES
+	----------
+	Parametres : au format [a,b,c,d1], définissant l'équation du second plan.
+	
+	'''
     a,b,c,d=tuple(Parametres)
 
     x0,y0,z0=tuple(CentreSphere)
@@ -191,6 +216,20 @@ def DeterminerPlanSuivant(CentreSphere,Parametres,EPAISSEUR):
 
 def PremierPlanProchaineTranche(CentreSphere,Parametres,Decalage=1):
     
+	'''
+	A partir du premier plan d'une tranche, définit les paramètres d'un plan parallèle, à partir duquel sera définie la prochaine tranche.
+	
+	ARGUMENTS
+	----------
+	CentreSphere : coordonnées du centre de la sphère définie plus haut, au format [x0,y0,z0].
+	Parametres : parametres au format [a,b,c,d] de l'équation définissant le premier plan de la tranche précédente.
+	Decalage : distance entre les deux plans parallèles.
+	
+	SORTIES
+	----------
+	Parametres : au format [a,b,c,d1], définissant l'équation du premier plan de la tranche suivante.
+	
+	'''
     a,b,c,d=tuple(Parametres)
 
     x0,y0,z0=tuple(CentreSphere)
@@ -212,7 +251,21 @@ def PremierPlanProchaineTranche(CentreSphere,Parametres,Decalage=1):
 
 
 def EstEntre2Plans(Coord_Residue,ParametresA,ParametresB,EPAISSEUR):
-
+	'''
+	Détermine si un point donné se situe entre deux plans parallèles distincts d'une épaisseur donnée.
+	
+	ARGUMENTS
+	----------
+	Coord_Residue : coordonnées du point étudié, au format [xR,yR,zR].
+	ParametresA : parametres au format [a,b,c,d] de l'équation définissant le premier plan de la tranche.
+	ParametresB : parametres au format [a,b,c,d1] de l'équation définissant le second plan de la tranche.
+	EPAISSEUR : float définissant l'écart voulu entre les plans de chaque tranche.
+	
+	SORTIES
+	----------
+	Booléen : True si le point se situe entre les plans parallèles ; False sinon.
+	
+	'''
     if ParametresA[0:2]!=ParametresB[0:2] :
         return ('Error : Les deux plans ne sont pas parallèles')
     
@@ -233,13 +286,26 @@ def EstEntre2Plans(Coord_Residue,ParametresA,ParametresB,EPAISSEUR):
 
 def DistanceAuCentre(row): 
     '''
-    row est la ligne du dataframe correspondant au Residue
+	Donne la distance au centre de la sphère d'un point donné.
+	
+	ARGUMENTS
+	----------
+	row : ligne d'un Pandas.DataFrame contenant les colonnes 'x', 'y' et 'z' correspondant aux coordonnées du point étudié.
+	
+	SORTIES
+	----------
+	d : distance entre le point étudié et le centre de la sphère.
     '''
+	
     x0,y0,z0=tuple(centre_sphere)
     d=np.sqrt((row['x']-x0)**2+(row['y']-y0)**2+(row['z']-z0)**2)
     return d
 
 print(data)
+
+
+# Les lignes qui suivent définissent la distance entre le centre de la sphère et le Residue le plus éloigné.
+# Cela définit plus loin les tranches les plus éloignées du centre de la sphère.
 
 data=data.loc[data.hydrophobe==True]
 
@@ -248,11 +314,24 @@ data['DistanceAuCentre']=data.apply(DistanceAuCentre,axis=1)
 DistanceMax=np.abs(data['DistanceAuCentre'].max())
 
 def ListeTranches(Point):
-    
+    '''
+	Etablit une liste des tranches de l'espace (entre deux plans), parallèles entre elles et normales à un vecteur.
+	
+	ARGUMENTS
+	----------
+	Point : coordonnées au format [x0,y0,z0] d'un point de la sphère correspondant à l'extrémité du vecteur qui définit les plans.
+	
+	SORTIES
+	----------
+	Tranches : liste de coordonnées des deux plans formant chaque tranche, 
+	au format [E1,E], où E1 et E sont les paramètres de chaque plan, au format [a,b,c,d].
+    '''
     x0,y0,z0=tuple(centre_sphere)
+	
     #premier plan correspondant au vecteur:
     E0=EquationPlan(centre_sphere,Point)
     E1=DeterminerPlanSuivant(centre_sphere,E0, EPAISSEUR)
+	
     dTranche0=[E0,E1]
     
     Tranches=[dTranche0]
@@ -266,7 +345,7 @@ def ListeTranches(Point):
     
     DistancePlanBauCentre=np.abs(a*x0+b*y0+c*z0+d1)/np.sqrt(a**2+b**2+c**2)
     
-    while DistancePlanBauCentre<DistanceMax:
+    while DistancePlanBauCentre<DistanceMax: #DistanceMax est la distance au centre du Residue le plus éloigné.
         
         E=DeterminerPlanSuivant(centre_sphere,E1,EPAISSEUR)
         dTranche=[E1,E]
@@ -282,7 +361,20 @@ def ListeTranches(Point):
     return Tranches
 
 def ListeResiduesDansTranche(E0,E1):
-     
+    '''
+	A partir de deux plans définissant une tranche de l'espace, détermine les Residues compris entre ces deux plans.
+	
+	ARGUMENTS
+	----------
+	E0 : paramètres au format [a,b,c,d] du premier plan définissant la tranche.
+	E1 : paramètres au format [a,b,c,d] du second plan définissant la tranche.
+	
+	SORTIES
+	----------
+	ListeResidues : liste contenant les positions dans la chaîne des Residues compris entre les deux plans.
+	
+	'''
+	 
     ListeResidues=[]
     
     for index,row in data.iterrows():
@@ -298,16 +390,22 @@ def ListeResiduesDansTranche(E0,E1):
 #Resultats    
 grosse_liste=[]
 for index,row in points_sphere_coord.iterrows():
+
+	# Cette boucle parcourt les points de la sphère, et donc les vecteurs qu'ils définissent, 
+	# pour déterminer les Residues hydrophobes compris dans chaque tranche normale à ces vecteurs.
+	# Le résultat est une liste contenant tous les Residues trouvés dans les tranches en contenant le plus grand nombre pour chaque vecteur.
+	
     Point=[row['x'],row['y'],row['z']]
     Tranches=ListeTranches(Point)
     
     Residues=ListeResiduesDansTranche(Tranches[0][0],Tranches[0][1])
-    NombreMaxResiduesHydrophobes=[len(Residues)]
-    TrancheMax=[Tranches[0]]
-    ResiduesMax=[Residues]
+    NombreMaxResiduesHydrophobes=[len(Residues)] #Nombre maximal de Residues hydrophobes dans une tranche, selon un vecteur donné. Sera mis à jour dans la boucle suivante.
+    TrancheMax=[Tranches[0]] #Paramètres de la tranche contenant le plus grand nombre de Residues hydrophobes. Sera mis à jour dans la boucle suivante.
+    ResiduesMax=[Residues] #Liste des Residues contenus dans la tranche TrancheMax. Sera mis à jour dans la boucle suivante.
     
     for T in Tranches[1:] :
-
+	
+		# Cette boucle parcourt les tranches le long d'un vecteur donné pour définir celles qui contiennent le plus grand nombre de Residues hydrophobes.
         
         Residues=ListeResiduesDansTranche(T[0],T[1])
         
@@ -318,6 +416,7 @@ for index,row in points_sphere_coord.iterrows():
             
         else :
             if len(Residues)==NombreMaxResiduesHydrophobes[0]:
+				# Cette condition permet de tenir compte des cas où plusieurs tranches ont le nombre maximal de Residues hydrophobes le long d'un vecteur donné.
                 TrancheMax.append(T)
                 ResiduesMax.append(Residues)
     for i in range(len(ResiduesMax)):
@@ -345,7 +444,7 @@ print(liste_membrane_pot)
 for i in liste_membrane_pot:
 	print(len(i))
 
-listenontriee=[]
+listenontriee=[] #Liste de tous les Residues trouvés précédemment, non triés selon le vecteur normal à leur tranche.
 for i in range(len(liste_membrane_pot)):
 	for j in range(len(liste_membrane_pot[i])):
 		listenontriee.append(liste_membrane_pot[i][j])
@@ -355,6 +454,20 @@ print(sorted(listenontriee))
 
 
 def couleur(Arg):
+	'''
+	Détermine la couleur d'une donnée en fonction de sa valeur True ou False.
+	Cette fonction est utilisée pour donner une couleur rouge aux Residues obtenus dans cette étude, en vue de la réalisation de figures.
+	
+	ARGUMENTS
+	----------
+	Arg : Booléen True ou False.
+	
+	SORTIES
+	----------
+	'r' : définit la couleur rouge si l'entrée est 'True'.
+	'b' : définit la couleur bleue si l'entrée est 'False'.
+	
+	'''
 	if Arg == True:
 		return 'r'
 	else :
@@ -368,7 +481,10 @@ data_sauvegarde["couleur"]=data_sauvegarde.membrane.apply(couleur)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 #ax.scatter(data["x"],data["y"],data["z"])
+
+# Représentation de la protéine, les Residues hydrophobes sélectionnés précédemment sont en rouge.
 ax.scatter(data_sauvegarde["x"],data_sauvegarde["y"],data_sauvegarde["z"],c=data_sauvegarde["couleur"])
 plt.show()
 
+# Enregistrement des résultats au format csv
 data_membrane.drop("DistanceAuCentre",axis='columns').to_csv("membrane_resultats_{0}.csv".format(fichier_pdb[-8:-4]),sep=',',index=False)
